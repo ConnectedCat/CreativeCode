63d5f086eaf5488f9f80aec862eb4cb1e5c884fa
Maxim Safioulline <maxim@connectedcatmedia.com>
2020-07-05 20:44:27 -0700

    Built out an ecosystem based on mitosis

--- P5js/ModdedMitosis/scripts/cell.js
+++ P5js/ModdedMitosis/scripts/cell.js
@@ -1,34 +1,41 @@
-class Cell{
-    constructor(_rad = random(thresholdRad/5, thresholdRad/2), _pos = createVector(random(width), random(height))){
-        this.rad = _rad;
+class Cell {
+    constructor(_diam = random(thresholdDiam / 5, thresholdDiam / 2), _pos = createVector(random(width), random(height))) {
+        this.diam = _diam;
         this.noisePos = 0;
-        this.color = color(random(50, 200), random(255), 0, 120);
         this.pos = _pos;
 
         this.destiny = random([0, 1]);
-        console.log(this.destiny);
+        this.color = (this.destiny != 0) ? color(random(150, 255), random(100), 0, 120) : color(random(100), random(150, 255), 0, 120);
     }
 
-    move(){
-        this.pos.add(p5.Vector.random2D().mult(5));
-        this.pos.x = constrain(this.pos.x, this.rad, width - this.rad);
-        this.pos.y = constrain(this.pos.y, this.rad, height - this.rad);
+    move() {
+        this.pos.add(p5.Vector.random2D().mult(this.diam * 0.3));
+        this.pos.x = constrain(this.pos.x, this.diam, width - this.diam);
+        this.pos.y = constrain(this.pos.y, this.diam, height - this.diam);
     }
 
-    grow(){
+    grow() {
         this.noisePos += 0.05;
         let path = (this.destiny != 0) ? noise(this.noisePos, 0) : noise(0, this.noisePos);
-        this.rad += map(path, 0, 1, -1, 1.55);
+        this.diam += map(path, 0, 1, -1, 1.55);
     }
 
-    mitosis(){
-        let newRad = sqrt(this.rad);
-        this.rad = newRad;
-        return new Cell(newRad, this.pos.copy());
+    mitosis() {
+        let newDiam = sqrt(this.diam);
+        this.diam = newDiam;
+        return new Cell(newDiam, this.pos.copy());
     }
 
-    display(){
+    overlap(_pos, _diam) {
+        if (this.pos.dist(_pos) < (this.diam/2 + _diam/2)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    display() {
         fill(this.color);
-        ellipse(this.pos.x, this.pos.y, this.rad, this.rad);
+        ellipse(this.pos.x, this.pos.y, this.diam, this.diam);
     }
 }
--- P5js/ModdedMitosis/scripts/sketch.js
+++ P5js/ModdedMitosis/scripts/sketch.js
@@ -1,29 +1,115 @@
 let cells = [];
-let thresholdRad = 100;
+
+let thresholdDiam = 20;
+let deathCoeff = thresholdDiam * 0.3;
+let field;
+
+let lavaColor;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
 
+    lavaColor = color(50, 50, 50);
+    field = createNoiseMap(width, height);
+
     cells.push(new Cell);
     noStroke();
+
+    frameRate(10);
 }
 
 function draw() {
-    background(200);
+    image(field, 0, 0);
+    let neighbours = [];
 
     cells.forEach((element, index) => {
         element.grow();
-        if(element.rad > thresholdRad){
+        if (element.diam > thresholdDiam) {
             cells.push(element.mitosis());
         }
-        
-        element.move();
-        element.display();
 
-        if(element.rad < thresholdRad/20){
+        element.move();
+
+        // die of malnutrition/lava
+        if (element.diam < thresholdDiam / deathCoeff || isLava(element.pos.x, element.pos.y, field)) {
             cells.splice(index, 1);
         }
+        //check for neighbours
+        for (let j = index + 1; j < cells.length; j++) {
+            if(element.overlap(cells[j].pos, cells[j].diam) && neighbours.indexOf(index) < 0 ){
+                neighbours.push(index);
+                neighbours.push(j);
+            }
+        }
+
+        element.display();
     })
+
+    //die of overcrowding
+    neighbours.forEach((element) => {
+        if(random() < 0.03){
+            cells.splice(element, 1);
+        }
+    })
+
+    if (cells.length < 4 || random() < 0.1) {
+        spontaneouslySeed();
+    }
+
+
+}
+
+// LOCAL HELPERS:
+
+function keyPressed() {
+    switch (key) {
+        case 's':
+            noLoop();
+            break;
+        case 'r':
+            loop();
+            break;
+        default:
+            break;
+    }
+}
+
+function createNoiseMap(_w, _h) {
+    let c;
+    let mapImg = createImage(_w, _h);
+    mapImg.loadPixels();
+
+
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+            if (noise(x * 0.01, y * 0.01) > 0.4) {
+                mapImg.set(x, y, color(50, 100, 200));
+            } else {
+                mapImg.set(x, y, lavaColor);
+            }
+
+        }
+    }
+    mapImg.updatePixels();
+    noiseSeed();
+
+    return mapImg;
+
+}
+
+function spontaneouslySeed() {
+    if (random() > 0.6) {
+        cells.push(new Cell);
+    }
+}
+
+function isLava(_locX, _locY, _image) {
+    let currentColor = color(_image.get(_locX, _locY));
+    if (currentColor.toString() === lavaColor.toString()) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 // HELPER STUFF:
